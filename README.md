f78817026c381e22f8fae5caaf54e467125beb79
mpbraun2 <mpbraun2@gmail.com>
2017-07-09 15:10:30 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# blogger_mike_braun
---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ActiveRecord::Base
+  has_many :messages
+  belongs_to :blog
+  validates :content, presence: true
+  validates :title, length: { minimum: 7 }
+end
